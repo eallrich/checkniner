06ab4cb03a8e86c1a07172c1469e0da9f3f420bf
Evan Allrich <evan@unguku.com>
2013-03-30 16:10:56 +0000

    Configuring django-debug-toolbar

--- cotracker/cotracker/settings/development.py
+++ cotracker/cotracker/settings/development.py
@@ -5,3 +5,9 @@ TEMPLATE_DEBUG = DEBUG
 
 ADMINS = ()
 MANAGERS = ADMINS
+
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+
+INSTALLED_APPS += ('debug_toolbar',)
+
+INTERNAL_IPS = (get_env_var('INTERNAL_IP'),)
