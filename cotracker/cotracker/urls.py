770a62977ba4b9faeb965f6c1e00571657fd69b2
Evan Allrich <evan@unguku.com>
2013-03-30 18:19:10 +0000

    Regex fix

--- cotracker/cotracker/urls.py
+++ cotracker/cotracker/urls.py
@@ -10,5 +10,5 @@ urlpatterns = patterns('',
 
 if settings.SERVE_STATIC:
     urlpatterns += patterns('',
-	url(r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
+	url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT }),
     )
