da0ec265d15e7968800afbe2cbf8705cdedc94a0
Evan Allrich <evan@unguku.com>
2013-03-31 01:53:42 +0000

    Initial settings for a test environment

---
+++ cotracker/cotracker/settings/test.py
@@ -0,0 +1,15 @@
+from .base import *
+
+# Test Settings
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
+TEST_DISCOVER_ROOT = PROJECT_ROOT
+TEST_DISCOVER_PATTERN = "*"
+
+# In-memory database for speed
+DATABASES = {
+    "default": {
+	"ENGINE": "django.db.backends.sqlite3",
+	"NAME": ":memory:",
+    },
+}
