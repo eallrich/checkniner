a02792bc15f3e63bed016d11f1de0aad2dfb7388
Evan Allrich <evan@unguku.com>
2013-03-31 12:25:00 +0000

    Modular reusable code is good

--- cotracker/checkouts/tests/test_models.py
+++ cotracker/checkouts/tests/test_models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+import datetime
 
 from django.test import TestCase
 from django.contrib.auth.models import Group, User
@@ -6,71 +6,68 @@ from django.contrib.auth.models import Group, User
 from checkouts.models import AircraftType, Airstrip, Checkout
 
 
+def create_aircrafttype(name='FooBar1234'):
+    o = AircraftType.objects.create(name=name)
+    return (o, {'name': name,})
+
+
+def create_airstrip(ident='ABCD', name='AlphaBravoCharlieDelta'):
+    o = Airstrip.objects.create(ident=ident, name=name)
+    return (o, {'ident': ident, 'name': name,})
+
+
+def create_pilot(username='kimpilot', email='kim@ex.com', password='secret', first_name='Kim', last_name='Pilot'):
+    pilot_group, _ = Group.objects.get_or_create(name='Pilots')
+    pilot = User.objects.create_user(username, email, password)
+    pilot.groups.add(pilot_group)
+    pilot.first_name = first_name
+    pilot.last_name = last_name
+    
+    attributes = {
+	'username': username,
+	'email': email,
+	'password': password,
+	'first_name': first_name,
+	'last_name': last_name,
+    }
+    
+    return (pilot, attributes)
+
+
 class AircraftTypeTests(TestCase):
     
     def test_unicode(self):
-	name = 'FooBar1234'
-	o = AircraftType.objects.create(name=name)
-	self.assertEqual(o.__unicode__(), name)
+	o, attributes = create_aircrafttype()
+	self.assertEqual(o.__unicode__(), attributes['name'])
 
 
 class AirstripTests(TestCase):
     
     def test_unicode(self):
-	ident, name = ('ABCD', 'AlphaBravoCharlieDelta')
-	o = Airstrip.objects.create(ident=ident, name=name)
-	self.assertEqual(o.__unicode__(), "%s (%s)" % (ident, name)) 
+	o, attributes = create_airstrip()
+	self.assertEqual(
+	    o.__unicode__(), 
+	    "%s (%s)" % (attributes['ident'], attributes['name'])
+	) 
 
 
 class CheckoutTests(TestCase):
     
     def setUp(self):
-	# Properties
-	self.group_name = 'Pilots'
-	
-	self.pilot_username = 'kimpilot'
-	self.pilot_email = 'kimpilot@example.com'
-	self.pilot_password = 'super password'
-	self.pilot_first_name = 'Kim'
-	self.pilot_last_name = 'Pilot'
-	
-	self.aircrafttype_name = 'FooBar1234'
-	self.airstrip_ident = 'ABCD'
-	self.airstrip_name = 'AlphaBravoCharlieDelta'
-	
-	# Objects
-	group = Group.objects.create(name=self.group_name)
-	
-	self.pilot = User.objects.create_user(
-	    self.pilot_username, 
-	    self.pilot_email, 
-	    self.pilot_password
-	)
-	self.pilot.groups.add(group)
-	self.pilot.first_name = self.pilot_first_name
-	self.pilot.last_name = self.pilot.last_name
-	
-	self.aircraft_type = AircraftType.objects.create(name=self.aircrafttype_name)
-	
-	self.airstrip = Airstrip.objects.create(
-	    ident=self.airstrip_ident,
-	    name=self.airstrip_name
-	)
+	self.pilot, self.pilot_attr = create_pilot()
+	self.aircrafttype, self.ac_attr = create_aircrafttype()
+	self.airstrip, self.airstrip_attr = create_airstrip()
+	self.date = datetime.date(2013,03,31)
 	
 	self.checkout = Checkout.objects.create(
 	    pilot = self.pilot,
-	    aircraft_type = self.aircraft_type,
+	    aircraft_type = self.aircrafttype,
 	    airstrip = self.airstrip,
-	    date = datetime.now(),
+	    date = self.date,
 	)
-	
-	# We'll reuse the date during testing. Since the tests will (very)
-	# likely complete on the same day they were started, this shouldn't
-	# cause problems.
-	self.date = self.checkout.date
     
     def test_unicode(self):
-	expected = '%s was checked out at %s in a %s on %s' % (self.checkout.get_pilot_name(), self.airstrip, self.aircraft_type, self.date) 
+	expected = '%s was checked out at %s in a %s on %s' % (self.checkout.get_pilot_name(), self.airstrip, self.aircrafttype, self.date) 
 	self.assertEqual(self.checkout.__unicode__(), expected)
     
     def test_get_pilot_name(self):
