38214174ca7d537ef3a38c843a1dbb0309a49163
Evan Allrich <evan@unguku.com>
2013-03-31 01:56:14 +0000

    Correction to the test pattern

--- cotracker/cotracker/settings/test.py
+++ cotracker/cotracker/settings/test.py
@@ -4,7 +4,7 @@ from .base import *
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
 TEST_DISCOVER_TOP_LEVEL = PROJECT_ROOT
 TEST_DISCOVER_ROOT = PROJECT_ROOT
-TEST_DISCOVER_PATTERN = "*"
+TEST_DISCOVER_PATTERN = "test*"
 
 # In-memory database for speed
 DATABASES = {
