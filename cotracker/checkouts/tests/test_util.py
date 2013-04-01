98aae8e54e16ffc873ea56c1bf0c33e3aea0703b
Evan Allrich <evan@unguku.com>
2013-04-01 00:49:54 +0000

    Enhancing reuse, plus tests!

--- cotracker/checkouts/tests/test_util.py
+++ cotracker/checkouts/tests/test_util.py
@@ -1,26 +1,30 @@
+import datetime
+
 from django.test import TestCase
 
 from checkouts import util
-from checkouts.models import AircraftType
+from checkouts.models import AircraftType, Checkout
+
+import helper
 
 
-class UtilTests(TestCase):
+class GetAircraftTypeNamesTests(TestCase):
     
-    def test_get_aircrafttype_names_empty(self):
+    def test_empty(self):
 	self.assertEqual(len(util.get_aircrafttype_names()), 0)
     
-    def test_get_aircrafttype_names_single(self):
+    def test_single(self):
 	name = 'ACTypeName'
 	AircraftType.objects.create(name=name)
 	self.assertEqual(util.get_aircrafttype_names(), [name,])
     
-    def test_get_aircrafttype_names_multi(self):
+    def test_multiple(self):
 	names = ['Name1','Name2','Name3']
 	for n in names:
 	    AircraftType.objects.create(name=n)
 	self.assertEqual(util.get_aircrafttype_names(), names)
     
-    def test_get_aircrafttype_names_sort(self):
+    def test_multiple_sort(self):
 	names = ['Name2','Name1','Name3']
 	for n in names:
 	    AircraftType.objects.create(name=n)
@@ -31,3 +35,137 @@ class UtilTests(TestCase):
 	# Descending
 	names.reverse()
 	self.assertEqual(util.get_aircrafttype_names("-name"), names)
+
+
+class PilotCheckoutsGroupedByAirstripTests(TestCase):
+    
+    def test_empty(self):
+	pilot, _ = helper.create_pilot()
+	self.assertEqual(len(util.pilot_checkouts_grouped_by_airstrip(pilot)), 0)
+    
+    def test_single_airstrip_single_aircrafttype(self):
+	pilot, _ = helper.create_pilot()
+	airstrip, _ = helper.create_airstrip()
+	actype, _ = helper.create_aircrafttype()
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip,
+	    aircraft_type=actype,
+	    date=datetime.datetime.now(),
+	)
+	
+	expected = [{
+	    'ident': airstrip.ident,
+	    'name': airstrip.name,
+	    'aircraft': [True,],
+	},]
+	
+	by_airstrip = util.pilot_checkouts_grouped_by_airstrip(pilot)
+	self.assertEqual(by_airstrip, expected)
+    
+    def test_single_airstrip_multiple_aircrafttype(self):
+	pilot, _ = helper.create_pilot()
+	airstrip, _ = helper.create_airstrip()
+	actype1, _ = helper.create_aircrafttype('Name1')    
+	actype2, _ = helper.create_aircrafttype('Name2')
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip,
+	    aircraft_type=actype1,
+	    date=datetime.datetime.now(),
+	)
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip,
+	    aircraft_type=actype2,
+	    date=datetime.datetime.now(),
+	)
+	
+	expected = [{
+	    'ident': airstrip.ident,
+	    'name': airstrip.name,
+	    'aircraft': [True,True],
+	},]
+	
+	by_airstrip = util.pilot_checkouts_grouped_by_airstrip(pilot)
+	self.assertEqual(by_airstrip, expected)
+	
+    def test_multiple_airstrip_single_aircrafttype(self):
+	pilot, _ = helper.create_pilot()
+	airstrip1, _ = helper.create_airstrip('ID1','Airstrip1')
+	airstrip2, _ = helper.create_airstrip('ID2','Airstrip2')
+	actype, _ = helper.create_aircrafttype()    
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip1,
+	    aircraft_type=actype,
+	    date=datetime.datetime.now(),
+	)
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip2,
+	    aircraft_type=actype,
+	    date=datetime.datetime.now(),
+	)
+	
+	expected = [
+	    {
+		'ident': airstrip1.ident,
+		'name': airstrip1.name,
+		'aircraft': [True,],
+	    }, {
+		'ident': airstrip2.ident,
+		'name': airstrip2.name,
+		'aircraft': [True,],
+	    },
+	]
+	
+	by_airstrip = util.pilot_checkouts_grouped_by_airstrip(pilot)
+	self.assertEqual(by_airstrip, expected)
+	
+    def test_multiple_airstrip_multiple_aircrafttype(self):
+	pilot, _ = helper.create_pilot()
+	airstrip1, _ = helper.create_airstrip('ID1','Airstrip1')
+	airstrip2, _ = helper.create_airstrip('ID2','Airstrip2')
+	actype1, _ = helper.create_aircrafttype('AC1')
+	actype2, _ = helper.create_aircrafttype('AC2')
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip1,
+	    aircraft_type=actype1,
+	    date=datetime.datetime.now(),
+	)
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip1,
+	    aircraft_type=actype2,
+	    date=datetime.datetime.now(),
+	)
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip2,
+	    aircraft_type=actype1,
+	    date=datetime.datetime.now(),
+	)
+	Checkout.objects.create(
+	    pilot=pilot,
+	    airstrip=airstrip2,
+	    aircraft_type=actype2,
+	    date=datetime.datetime.now(),
+	)
+	
+	expected = [
+	    {
+		'ident': airstrip1.ident,
+		'name': airstrip1.name,
+		'aircraft': [True,True],
+	    }, {
+		'ident': airstrip2.ident,
+		'name': airstrip2.name,
+		'aircraft': [True,True],
+	    },
+	]
+	
+	by_airstrip = util.pilot_checkouts_grouped_by_airstrip(pilot)
+	self.assertEqual(by_airstrip, expected)
+	
--- cotracker/checkouts/util.py
+++ cotracker/checkouts/util.py
@@ -1,4 +1,4 @@
-from .models import AircraftType
+from .models import AircraftType, Checkout
 
 
 def get_aircrafttype_names(order="name"):
@@ -6,3 +6,42 @@ def get_aircrafttype_names(order="name"):
     AircraftTypes"""
     aircrafttypes = AircraftType.objects.order_by(order)
     return [actype.name for actype in aircrafttypes]
+
+
+def pilot_checkouts_grouped_by_airstrip(pilot):
+    """Organizes the pilot's checkouts by airstrips.
+    
+    Returns a list (sorted by airstrip ident) in which
+    every airstrip at which the given pilot is checked 
+    out is a dictionary, with a key:value pair indicating
+    whether the pilot is checked out or not in each
+    AircraftType."""
+    actypes = get_aircrafttype_names()
+
+    pilot_checkouts = Checkout.objects.filter(pilot=pilot).select_related('airstrip', 'aircraft_type')
+    pilot_checkouts = pilot_checkouts.order_by('airstrip__ident', 'aircraft_type__name')
+    
+    by_airstrip = []
+    row_data = None
+    for c in pilot_checkouts:
+	if row_data == None or c.airstrip.ident != row_data['ident']:
+	    # Don't save on the initial loop iteration
+	    if row_data != None:
+		by_airstrip.append(row_data)
+	    
+	    row_data = {
+		'ident': c.airstrip.ident,
+		'name': c.airstrip.name,
+		'aircraft': [False,] * len(actypes),
+	    }
+	
+	ac_index = actypes.index(c.aircraft_type.name)
+	row_data['aircraft'][ac_index] = True
+    
+    # Saving the very last airstrip record is missed by 
+    # the 'is this the same airstrip as before?' check,
+    # so we'll manually save it to the list (if necessary)
+    if row_data != None:
+        by_airstrip.append(row_data)
+    
+    return by_airstrip
