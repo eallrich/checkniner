035f18349e3a17e9f4553c76072201c77aef12ad
Evan Allrich <evan@unguku.com>
2013-04-03 01:58:08 +0000

    Adding a function for grouping airstrip checkouts

--- cotracker/checkouts/util.py
+++ cotracker/checkouts/util.py
@@ -19,8 +19,7 @@ CHECKOUT_UNPRECEDENTED = "unprecedented"
 
 
 def get_aircrafttype_names(order="name"):
-    """Populates a sorted list with the names of all known
-    AircraftTypes"""
+    """Populates a sorted list with the names of all known AircraftTypes"""
     aircrafttypes = AircraftType.objects.order_by(order)
     return [actype.name for actype in aircrafttypes]
 
@@ -28,11 +27,9 @@ def get_aircrafttype_names(order="name"):
 def pilot_checkouts_grouped_by_airstrip(pilot):
     """Organizes the pilot's checkouts by airstrips.
     
-    Returns a list (sorted by airstrip ident) in which
-    every airstrip at which the given pilot is checked 
-    out is a dictionary, with a key:value pair indicating
-    whether the pilot is checked out or not in each
-    AircraftType."""
+    Returns a list (sorted by airstrip ident) in which every airstrip at which
+    the given pilot is checked out is a dictionary, with a key:value pair 
+    indicating whether the pilot is checked out or not in each AircraftType."""
     actypes = get_aircrafttype_names()
 
     pilot_checkouts = Checkout.objects.filter(pilot=pilot).select_related('airstrip', 'aircraft_type')
@@ -62,3 +59,42 @@ def pilot_checkouts_grouped_by_airstrip(pilot):
         by_airstrip.append(row_data)
     
     return by_airstrip
+
+
+def airstrip_checkouts_grouped_by_pilot(airstrip):
+    """Organizes the airstrip's checkouts by pilot.
+    
+    Returns a list (sorted by pilot last name then first name) in which every
+    pilot checked out at the given airstrip is a dictionary, with a key:value
+    pair indicating whether the pilot is checked out or not in each 
+    AircraftType.
+    """
+    actypes = get_aircrafttype_names()
+    
+    airstrip_checkouts = Checkout.objects.filter(airstrip=airstrip).select_related('pilot', 'aircraft_type')
+    airstrip_checkouts = airstrip_checkouts.order_by('pilot__last_name', 'pilot__first_name', 'aircraft_type__name')
+    
+    by_pilot = []
+    row_data = None
+    for c in airstrip_checkouts:
+	if row_data == None or c.pilot.username != row_data['pilot_username']:
+	    # Don't save on the initial loop iteration
+	    if row_data != None:
+		by_pilot.append(row_data)
+	    
+	    row_data = {
+		'pilot_name': c.get_pilot_name(),
+		'pilot_username': c.pilot.username,
+		'aircraft': [CHECKOUT_BELUM,] * len(actypes),
+	    }
+	
+	ac_index = actypes.index(c.aircraft_type.name)
+	row_data['aircraft'][ac_index] = CHECKOUT_SUDAH
+    
+    # Saving the very last airstrip record is missed by 
+    # the 'is this the same airstrip as before?' check,
+    # so we'll manually save it to the list (if necessary)
+    if row_data != None:
+        by_pilot.append(row_data)
+    
+    return by_pilot
