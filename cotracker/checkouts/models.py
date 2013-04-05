99c172f67a11817274f7529d5d8e102a1f0e3fa8
Evan Allrich <evan@unguku.com>
2013-04-05 02:23:22 +0000

    Instances can report attached/unattached airstrips

--- cotracker/checkouts/models.py
+++ cotracker/checkouts/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 from model_utils.models import TimeStampedModel
 
+
 class Airstrip(TimeStampedModel):
     ident = models.CharField(max_length=4, unique=True)
     name = models.CharField(max_length=255)
@@ -11,12 +12,27 @@ class Airstrip(TimeStampedModel):
 
     def __unicode__(self):
         return "%s (%s)" % (self.ident, self.name)
+    
+    def attached_airstrips(self):
+	"""Retrieves the set of Airstrips which reference this instance as a
+	base
+	"""
+	return Airstrip.objects.filter(bases=self).order_by('ident')
+    
+    def unattached_airstrips(self):
+	"""Retrieves the set of Airstrips which do not reference this instance
+	as a base
+	"""
+	# Note that the 'self' instance is specifically removed from the list
+	return Airstrip.objects.exclude(pk=self.pk).exclude(bases=self).order_by('ident')
+
 
 class AircraftType(TimeStampedModel):
     name = models.CharField(max_length=10)
 
     def __unicode__(self):
         return self.name
+
 
 class Checkout(TimeStampedModel):
     pilot = models.ForeignKey(User, limit_choices_to={'groups__name': 'Pilots'})
