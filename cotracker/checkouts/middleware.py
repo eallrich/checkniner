94574fc652ba6b8de7baa597360e089ecd65997b
Evan Allrich <evan@unguku.com>
2013-07-01 01:40:17 +0000

    Explicitly remove UserIsPilotPatch from middleware
    It would only run once (via __init__) anyway, but this way we can wrap things
    up nicely.

--- cotracker/checkouts/middleware.py
+++ cotracker/checkouts/middleware.py
@@ -3,6 +3,7 @@
 import logging
 
 from django.contrib.auth.models import User
+from django.core.exceptions import MiddlewareNotUsed
 
 import util
 
@@ -15,8 +16,11 @@ class UserIsPilotPatch():
     Because we only need to patch the model once (when an interpreter begins)
     we can make use of the __init__ method: This method is called just once
     for middleware, when the interpreter responds to the first request. [0]
+    Once the User model is patched, we can completely remove ourselves from the
+    middleware process by raising MiddlewareNotUsed. [1]
     
     [0] https://docs.djangoproject.com/en/1.5/topics/http/middleware/#init
+    [1] https://docs.djangoproject.com/en/1.5/topics/http/middleware/#marking-middleware-as-unused
     """
     
     def __init__(self):
@@ -29,5 +33,10 @@ class UserIsPilotPatch():
 	>>> foo = User.objects.get(pk=1)
 	>>> foo.is_pilot() # foo.is_pilot(self), becomes util.is_pilot(foo)
 	"""
-	logger.info("Patching built-in User model with 'is_pilot()'...")
+	logger.info("Patching built-in User model with 'is_pilot()'")
 	User.is_pilot = util.is_pilot
+	
+	# We're not interested in seeing future requests/responses, so let 
+	# Django know that we can be disabled.
+	logger.debug("Removing 'UserIsPilotPatch' from the middleware process")
+	raise MiddlewareNotUsed()
