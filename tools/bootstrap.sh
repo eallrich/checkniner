f4ddc68cb0811cda98fa08d7a6cdee0bc1209da1
Evan Allrich <evan@unguku.com>
2013-07-08 01:44:07 +0000

    Provide scripts for an automatic deploy process

---
+++ tools/bootstrap.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# ============================================================================
+# Bootstrap deployment of checkniner
+# ----------------------------------
+# Requires ssh access and sudo privileges on the target
+# 
+# Begins by installing a few core packages, then continues with updating the
+# rest of the system. Once a clone of the repository is available, it
+# kickstarts the full deployment script to complete the process.
+#
+# Usage:
+#   ./bootstrap.sh ssh-connection hosted-domain sentry-dsn
+#   E.g.
+#   ./bootstrap.sh ubuntu@example.com example.com https://access:key@example.com/2
+# ============================================================================
+
+CONNECTION=$1
+ALLOWED_HOST=$2
+SENTRY_DSN=$3
+
+# + byobu because it's awesome and if I have to SSH in I'll want it
+# + git to retrieve the repository
+# + language-pack-en to resolve locale issues in some environments (e.g. LXC)
+CORE="byobu git language-pack-en"
+
+REPOSITORY=https://github.com/eallrich/checkniner.git
+SSH_ID=~/.ssh/id_rsa.pub
+
+# For sudo on the remote
+read -s -p "Password: " PASSWORD
+echo ''
+
+ssh-copy-id -i $SSH_ID $CONNECTION
+
+echo $PASSWORD | ssh $CONNECTION "sudo -S apt-get update"
+echo $PASSWORD | ssh $CONNECTION "sudo -S apt-get install $CORE --assume-yes"
+echo $PASSWORD | ssh $CONNECTION "sudo -S apt-get dist-upgrade --assume-yes"
+
+ssh $CONNECTION "git clone $REPOSITORY"
+
+echo ""
+echo "Bootstrap complete, initiating fresh deployment"
+
+echo $PASSWORD | ssh $CONNECTION "~/checkniner/tools/fresh.sh $ALLOWED_HOST $SENTRY_DSN"
+
+echo ""
+echo "Done!"
---
+++ tools/fresh.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# ============================================================================
+# Configures a fresh checkniner server
+# ------------------------------------
+# Requires sudo
+#
+# + Installs OS package dependencies
+# + Installs python packages to a virtualenv
+# + Creates a user and database in postgres
+# + Sets up environment variables in the virtualenv
+# + Instructs django to prepare the database and static files
+# + Configures supervisor to manage gunicorn
+# + Configures nginx to reverse proxy for gunicorn
+# 
+# Usage:
+#   ./fresh.sh hosted-domain sentry-dsn
+#   E.g.
+#   ./fresh.sh example.com https://access:key@example.com/2
+# ============================================================================
+
+ALLOWED_HOST=$1
+SENTRY_DSN=$2
+
+PG_DATABASE=checkniner
+PG_USERNAME=`whoami`
+SECRET_KEY=$(python -c "import base64, os; print base64.b64encode(os.urandom(40))")
+SITE_ROOT=~/checkniner/
+
+# For sudo
+# When called by bootstrap.sh, the password is provided automatically
+read -s -p "Password: " PASSWORD
+echo ''
+
+DEPENDENCIES="python-virtualenv python-dev postgresql libpq-dev nginx supervisor"
+echo $PASSWORD | sudo -S apt-get install $DEPENDENCIES --assume-yes
+
+# Make sure we've lost sudo so that we don't mess up a question from postgres
+# by providing the password when it wasn't expected.
+sudo -k
+
+echo $PASSWORD | sudo -S -u postgres /bin/bash -c "echo ''; echo \"CREATE DATABASE $PG_DATABASE;\\q\" | psql"
+echo $PASSWORD | sudo -S -u postgres /bin/bash -c "echo ''; echo \"CREATE USER $PG_USERNAME;\\q\" | psql"
+echo $PASSWORD | sudo -S -u postgres /bin/bash -c "echo ''; echo \"GRANT ALL PRIVILEGES ON DATABASE $PG_DATABASE TO $PG_USERNAME;\\q\" | psql"
+
+cd $SITE_ROOT
+virtualenv .
+source bin/activate
+pip install -r requirements.txt
+
+echo "export ALLOWED_HOST=$ALLOWED_HOST" >> bin/activate
+echo "export DATABASE_URL=postgres://$PG_USERNAME@/$PG_DATABASE" >> bin/activate
+echo "export DJANGO_SETTINGS_MODULE=cotracker.settings.production" >> bin/activate
+echo "export PYTHONPATH=$SITE_ROOT/cotracker/" >> bin/activate
+echo "export SECRET_KEY=$SECRET_KEY" >> bin/activate
+echo "export SENTRY_DSN=$SENTRY_DSN" >> bin/activate
+
+deactivate
+source bin/activate
+
+# Note that we're not creating a superuser here. If one is needed and initial
+# data won't provide one when imported, run manage.py createsuperuser
+python cotracker/manage.py syncdb --noinput --no-initial-data
+python cotracker/manage.py migrate
+python cotracker/manage.py collectstatic --noinput
+
+# TODO: Supervisor setup
+
+# TODO: Nginx setup
+
+echo ""
+echo "Fresh deployment script complete"
+
