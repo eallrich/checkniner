fa05832290b1cffbf95c24ce94c0c1fc39e8b03a
Evan Allrich <evan@unguku.com>
2019-06-25 21:38:35 -0500

    More unicode-to-string conversions

--- cotracker/checkouts/tests/test_models.py
+++ cotracker/checkouts/tests/test_models.py
@@ -15,7 +15,7 @@ class AircraftTypeTests(TestCase):
     
     def test_unicode(self):
         o, attributes = helper.create_aircrafttype(object_only=False)
-        self.assertEqual(o.__unicode__(), attributes['name'])
+        self.assertEqual(str(o), attributes['name'])
 
 
 class AirstripTests(TestCase):
@@ -23,7 +23,7 @@ class AirstripTests(TestCase):
     def test_unicode(self):
         o, attributes = helper.create_airstrip(object_only=False)
         self.assertEqual(
-            o.__unicode__(), 
+            str(o),
             "%s (%s)" % (attributes['ident'], attributes['name'])
         ) 
     
@@ -161,4 +161,4 @@ class VerifyPatchedUserModelTests(TestCase):
     
     def test_unicode(self):
         expected = '%s, %s' % (self.pilot.last_name, self.pilot.first_name)
-        self.assertEqual(self.pilot.__unicode__(), expected)
+        self.assertEqual(str(self.pilot), expected)
